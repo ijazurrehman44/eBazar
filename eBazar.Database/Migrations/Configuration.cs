6f1a4ccb723ee9da7497d439c225936bd9d034b8
ijazurrehman44 <110520908+ijazurrehman44@users.noreply.github.com>
2022-08-11 11:00:32 +0500

    Add files via upload

---
+++ eBazar.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace eBazar.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eBazar.Database.eBazarContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(eBazar.Database.eBazarContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ eBazar.Database/eBazarContext.cs
@@ -0,0 +1,20 @@
+﻿using eBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eBazar.Database
+{
+    public class eBazarContext : DbContext
+    {
+        public eBazarContext() : base("eBazarConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
